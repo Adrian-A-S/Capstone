974f5c99aae454b0c348c3fe198ac5e30107e547
Adrian <54789923+Adrian-A-S@users.noreply.github.com>
2020-05-23 11:30:55 +0300

    First upload

---
+++ setup.sh
@@ -0,0 +1,8 @@
+AUTH0_DOMAIN='dev-y25we54i.eu.auth0.com'
+export AUTH0_DOMAIN
+API_AUDIENCE='ZiCapstone'
+export API_AUDIENCE
+ALGORITHMS=['RS256']
+export ALGORITHMS
+DATABASE_URL=postgres://yvytsxxrprnnpg:3fc1dabf74331d1296d25c9b468d68408adf5c34ce69a3c953ba0abdf5e16a62@ec2-34-225-82-212.compute-1.amazonaws.com:5432/d4hn7i3kafbsp6
+export DATABASE_URL
---
+++ test_app.py
@@ -0,0 +1,143 @@
+import unittest
+import json
+from app import create_app
+from flask_sqlalchemy import SQLAlchemy
+import pdb
+
+
+class Tests (unittest.TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.app.testing = True
+        self.client = self.app.test_client
+
+        with self.app.app_context():
+            self.db = SQLAlchemy(self.app)
+            self.db.init_app(self.app)
+
+        pdb.set_trace()
+        self.director = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkpDUFB4bUYzOWFhWTAyNUt2aWlrQSJ9.eyJpc3MiOiJodHRwczovL2Rldi15MjV3ZTU0aS5ldS5hdXRoMC5jb20vIiwic3ViIjoiMTJRVzhocFBkQnpsdEZ6OWFhU3kwVWQ5WENtcFRqMGJAY2xpZW50cyIsImF1ZCI6IlppQ2Fwc3RvbmUiLCJpYXQiOjE1OTAyMTE4NDAsImV4cCI6MTU5MDI5ODI0MCwiYXpwIjoiMTJRVzhocFBkQnpsdEZ6OWFhU3kwVWQ5WENtcFRqMGIiLCJzY29wZSI6ImdldDptb3ZpZXMgZ2V0OmFjdG9ycyBwb3N0Om1vdmllcyBwb3N0OmFjdG9ycyBwYXRjaDptb3ZpZXMgcGF0Y2g6YWN0b3JzIGRlbGV0ZTptb3ZpZXMgZGVsZXRlOmFjdG9ycyIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyIsInBlcm1pc3Npb25zIjpbImdldDptb3ZpZXMiLCJnZXQ6YWN0b3JzIiwicG9zdDptb3ZpZXMiLCJwb3N0OmFjdG9ycyIsInBhdGNoOm1vdmllcyIsInBhdGNoOmFjdG9ycyIsImRlbGV0ZTptb3ZpZXMiLCJkZWxldGU6YWN0b3JzIl19.AxF0rUYLvWBHmPbJF4uJEUe1p1Aol3HfxKR3DC74ukxMlRV3Iy7UmWVQSjNNBDbVNbYVKk2pfS3Cw7tQQuUP6eBMjrVIMsuLYXNeD4FpXmMN2ROhsrR3EO6_kYzH6or2LWXsT6jryJcDZOQIKeoizfI4FGGqA2_vlg70rHi1_IgdMltqZN7Be4sH7VH17JS4Wp5NtQTBG6n9n1kBMvwzC32HXR91rBDzs5_GSedISQjfp-e_5oPT6W9uvkIaTcx1bFN_B1_B2LgAUwiiuJYpUDjWAj_8pipuEthJYRM1sNvd5g2VPY61wNVBtqJWpRyZPNylIZfDBsxe_VMUxyYu4A'
+
+        self.headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer '+ self.director}
+
+        self.newMovie = {
+            'title': 'Movie 1',
+            'movie_date': '2020-01-01T00:00:00'
+        }
+        self.newActor = {
+            'name': 'Actor 1',
+            'age': 22,
+            'gender': 'Unkown'
+        }
+        self.otherMovie = {
+            'title': 'Movie 1',
+            'movie_date': '2020-01-01T00:00:00',
+            'actors': self.newActor
+        }
+        self.otherActor = {
+            'name': 'Actor 1',
+            'age': 23,
+            'gender': 'Still Unkown'
+        }
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testGET(self):
+        res = self.client().get('/movies')
+        self.assertEqual(res.status_code, 401)
+
+        res = self.client().get(
+            '/movies',
+            headers=self.headers)
+        self.assertEqual(res.status_code, 200)
+
+        res = self.client().get('/actors')
+        self.assertEqual(res.status_code, 401)
+
+        res = self.client().get(
+            '/actors',
+            headers=self.headers)
+        self.assertEqual(res.status_code, 200)
+
+    def testPOST(self):
+        res = self.client().post('/movies', json=self.newMovie)
+        self.assertEqual(res.status_code, 401)
+
+        res = self.client().post('/actors', json=self.newActor)
+        self.assertEqual(res.status_code, 401)
+
+        res = self.client().post(
+            '/movies',
+            headers=self.headers,
+            json=self.newMovie)
+        self.assertEqual(res.status_code, 200)
+
+        res = self.client().post(
+            '/actors',
+            headers=self.headers,
+            json=self.newActor)
+        self.assertEqual(res.status_code, 200)
+
+    def testPATCH(self):
+        res = self.client().patch('/movies/1', json=self.otherMovie)
+        self.assertEqual(res.status_code, 401)
+
+        res = self.client().patch('/actors/1', json=self.otherActor)
+        self.assertEqual(res.status_code, 401)
+
+        res = self.client().patch(
+            '/movies/1',
+            headers=self.headers,
+            json=self.otherMovie)
+        self.assertEqual(res.status_code, 200)
+
+        res = self.client().patch(
+            '/actors/1',
+            headers=self.headers,
+            json=self.otherActor)
+        self.assertEqual(res.status_code, 200)
+
+        res = self.client().patch(
+            '/movies/5',
+            headers=self.headers,
+            json=self.otherMovie)
+        self.assertEqual(res.status_code, 404)
+
+        res = self.client().patch(
+            '/actors/5',
+            headers=self.headers,
+            json=self.otherActor)
+        self.assertEqual(res.status_code, 404)
+
+    def testDELETE(self):
+        res = self.client().delete('/movies/1')
+        self.assertEqual(res.status_code, 401)
+
+        res = self.client().delete('/actors/1')
+        self.assertEqual(res.status_code, 401)
+
+        res = self.client().delete(
+            '/movies/5',
+            headers=self.headers)
+        self.assertEqual(res.status_code, 404)
+
+        res = self.client().delete(
+            '/actors/5',
+            headers=self.headers)
+        self.assertEqual(res.status_code, 404)
+
+        res = self.client().delete(
+            '/movies/1',
+            headers=self.headers)
+        self.assertEqual(res.status_code, 200)
+
+        res = self.client().delete(
+            '/actors/1',
+            headers=self.headers)
+        self.assertEquals(res.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
