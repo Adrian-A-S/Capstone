51daa0ee2c7065aabca790231a342b8e1ddd2585
Adrian <54789923+Adrian-A-S@users.noreply.github.com>
2020-05-24 08:05:44 +0300

    Third upload

--- test_app.py
+++ test_app.py
@@ -13,15 +13,13 @@ class Tests (unittest.TestCase):
         self.app = create_app()
         self.app.testing = True
         self.client = self.app.test_client
-        setup_db(self.app)
 
         with self.app.app_context():
-            self.db = SQLAlchemy()
+            self.db = SQLAlchemy(self.app)
             self.db.init_app(self.app)
             self.db.create_all()
             
 
-        pdb.set_trace()
         self.director = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkpDUFB4bUYzOWFhWTAyNUt2aWlrQSJ9.eyJpc3MiOiJodHRwczovL2Rldi15MjV3ZTU0aS5ldS5hdXRoMC5jb20vIiwic3ViIjoiMTJRVzhocFBkQnpsdEZ6OWFhU3kwVWQ5WENtcFRqMGJAY2xpZW50cyIsImF1ZCI6IlppQ2Fwc3RvbmUiLCJpYXQiOjE1OTAyMTE4NDAsImV4cCI6MTU5MDI5ODI0MCwiYXpwIjoiMTJRVzhocFBkQnpsdEZ6OWFhU3kwVWQ5WENtcFRqMGIiLCJzY29wZSI6ImdldDptb3ZpZXMgZ2V0OmFjdG9ycyBwb3N0Om1vdmllcyBwb3N0OmFjdG9ycyBwYXRjaDptb3ZpZXMgcGF0Y2g6YWN0b3JzIGRlbGV0ZTptb3ZpZXMgZGVsZXRlOmFjdG9ycyIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyIsInBlcm1pc3Npb25zIjpbImdldDptb3ZpZXMiLCJnZXQ6YWN0b3JzIiwicG9zdDptb3ZpZXMiLCJwb3N0OmFjdG9ycyIsInBhdGNoOm1vdmllcyIsInBhdGNoOmFjdG9ycyIsImRlbGV0ZTptb3ZpZXMiLCJkZWxldGU6YWN0b3JzIl19.AxF0rUYLvWBHmPbJF4uJEUe1p1Aol3HfxKR3DC74ukxMlRV3Iy7UmWVQSjNNBDbVNbYVKk2pfS3Cw7tQQuUP6eBMjrVIMsuLYXNeD4FpXmMN2ROhsrR3EO6_kYzH6or2LWXsT6jryJcDZOQIKeoizfI4FGGqA2_vlg70rHi1_IgdMltqZN7Be4sH7VH17JS4Wp5NtQTBG6n9n1kBMvwzC32HXR91rBDzs5_GSedISQjfp-e_5oPT6W9uvkIaTcx1bFN_B1_B2LgAUwiiuJYpUDjWAj_8pipuEthJYRM1sNvd5g2VPY61wNVBtqJWpRyZPNylIZfDBsxe_VMUxyYu4A'
 
         self.headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer '+ self.director}
@@ -52,6 +50,7 @@ class Tests (unittest.TestCase):
 
     def testGET(self):
         res = self.client().get('/movies')
+        pdb.set_trace()
         self.assertEqual(res.status_code, 401)
 
         res = self.client().get(
